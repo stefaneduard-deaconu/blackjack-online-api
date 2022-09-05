e191bf162a2fd8e0292fe8c1768ad618fe62fa7d
Stefan-Eduard Deaconu <stefan@stefandeaconu.com>
2022-09-05 11:11:45 +0300

    added origin for heroku frontend

--- app.js
+++ app.js
@@ -24,7 +24,8 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
     cors: {
-        origin: "http://localhost:3000",
+        origin: ```http://localhost:3000
+                   http://real-blackjack-online.herokuapp.com:* ```,
         methods: ['GET', 'POST']
     }
 })
